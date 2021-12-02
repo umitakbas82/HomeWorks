2045ffdaf712a0b0f8f0e7a4fad4a2785c6071dd
umitakbas82 <umit.akbas@hotmail.com.tr>
2021-12-02 13:41:55 +0300

    Add project files.

---
+++ ClassMetotDemo/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    class Customer
+    {
+        public string CustomerName { get; set; }
+        public int CustomerLastname { get; set; }
+        public int CustomerId { get; set; }
+    }
+}
