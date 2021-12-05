d148433eb6ad8de9d7c430e85bc4d1ccf51e26fb
umitakbas82 <umit.akbas@hotmail.com.tr>
2021-12-05 16:16:21 +0300

    Product Class

---
+++ Product/Products.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Product
+{
+    class Products
+    {
+        public string ProductName { get; set; }
+        public int ProductId { get; set; }
+        public int ProductPrice { get; set; }
+        public int ProductInStock { get; set; }
+
+
+
+    }
+}
---
+++ Product/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Product
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Products product1 = new Products();
+            product1.ProductName = "Laptop";
+            product1.ProductPrice = 2000;
+            product1.ProductInStock = 50;
+            product1.ProductId = 5;
+
+
+            Products product2 = new Products();
+            product2.ProductName = "Mouse";
+            product2.ProductPrice = 100;
+            product2.ProductInStock = 50;
+            product2.ProductId = 3;
+
+            Products product3 = new Products();
+            product3.ProductName ="Klavye";
+            product3.ProductPrice = 150;
+            product3.ProductInStock = 60;
+            product3.ProductId = 7;
+
+
+            foreach (var urun in Products)
+            {
+
+                Console.WriteLine(urun.ProductName + " " + urun.ProductPrice + " " + urun.ProductInStock);
+
+
+
+            }
+
+        }
+    }
+}
