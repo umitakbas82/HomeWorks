c003f5319bed765272c5c7b164a3964500518eae
umitakbas82 <umit.akbas@hotmail.com.tr>
2021-12-06 23:34:17 +0300

    Dictionary

---
+++ Dictionery/MyDictionary.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDictionary
+{
+    class MyDictionary<K,V>
+    {
+        TKey[] _keys;
+        TValue[] _value;
+        public MyDictionary()
+        {
+            _keys = new K[0];
+            _value = new V[0];
+        }
+
+        public void MyDictionary.Add(TKey key, TValue value);
+
+
+    }
+}
